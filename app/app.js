f0587c6ef4de225997cc45a665d85f624a3a65da
Paul Cluver <paulcl@sahomeloans.com>
2015-03-06 15:26:32 +0200

    general update

--- app/app.js
+++ app/app.js
@@ -1,11 +1,9 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('muktiApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'muktiApp.view1'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('muktiApp.view1 module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('muktiApp.view1'));
 
   describe('view1 controller', function(){
 
