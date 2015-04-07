9843d85632b8c95ccd4debcc14366510ee3cb7c1
Paul Cluver <paulcl@sahomeloans.com>
2015-04-07 09:11:23 +0200

    Stuff

--- app/controllers/workshops/workshops.js
+++ app/controllers/workshops/workshops.js
@@ -4,8 +4,11 @@ angular.module('yogaApp.workshops', ['ui.router'])
 	
 	.controller('workshopCtrl', ['$scope', 'WorkshopService', function($scope, WorkshopService) 
 	{
-		$scope.sortOrder = 'customSort';
 		$scope.workshops = WorkshopService.getAllWorkshops();
+		$scope.orderByField = 'name';
+  		$scope.reverseSort = false;
+
+		//$scope.sortOrder = 'customSort';
 		// $scope.getAllWorkshops = function()
 		// {
 		// 	$scope.workshops =  WorkshopService.getAllWorkshops();
--- karma.conf.js
+++ karma.conf.js
@@ -4,11 +4,10 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'node_modules/angular/angular.js',
+      'node_modules/ui-router/angular-ui-router.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'app/controllers/**/*.js'
     ],
 
     autoWatch : true,
