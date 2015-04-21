9f715d46ad80247db63226a7cc8b287e7958e5e5
Paul Cluver <paulcl@sahomeloans.com>
2015-04-21 11:20:49 +0200

    Moved stuffs around

--- app/app.js
+++ app/app.js
@@ -30,52 +30,52 @@ var yogaApp =  angular.module('yogaApp', [
 	$stateProvider
 		.state('home', {
 			url: "/home",
-			templateUrl: "app/controllers/home/home.html",
+			templateUrl: "controllers/home/home.html",
 			controller: "homeCtrl"
 		})
 
 		.state('hatha', {
 			url: "/hatha",
-			templateUrl: "app/controllers/yoga/hatha/hatha.html"
+			templateUrl: "controllers/yoga/hatha/hatha.html"
 		})
 		
 		.state('ashtanga', {
 			url: "/ashtanga",
-			templateUrl: "app/controllers/yoga/ashtanga/ashtanga.html"
+			templateUrl: "controllers/yoga/ashtanga/ashtanga.html"
 		})
 		
 		.state('vinyasa', {
 			url: "/vinyasa",
-			templateUrl: "app/controllers/yoga/vinyasa/vinyasa.html"
+			templateUrl: "controllers/yoga/vinyasa/vinyasa.html"
 		})
 
 		.state('workshops', {
 			url: "/workshops",
-			templateUrl: "app/controllers/schedule/workshops/workshops.html"
+			templateUrl: "controllers/schedule/workshops/workshops.html"
 		})
 
 		.state('classes', {
 			url: "/classes",
-			templateUrl: "app/controllers/schedule/classes/classes.html"
+			templateUrl: "controllers/schedule/classes/classes.html"
 		})
 		
 		.state('contact', {
 			url: "/contact",
-			templateUrl: "app/controllers/user/contact/contact.html"
+			templateUrl: "controllers/user/contact/contact.html"
 		})
 
 		.state('login', {
 			url: "/login",
-			templateUrl: "app/controllers/user/login/login.html"
+			templateUrl: "controllers/user/login/login.html"
 		})
 
 		.state('logout', {
 			url: "/logout",
-			templateUrl: "app/controllers/user/logout/logout.html"
+			templateUrl: "controllers/user/logout/logout.html"
 		})
 
 		.state('register', {
 			url: "/register",
-			templateUrl: "app/controllers/user/register/register.html"
+			templateUrl: "controllers/user/register/register.html"
 		})
 });
--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -1,12 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['../app/controllers/*/*_test.js'],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  baseUrl: 'http://localhost:82/#',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  }
+  specs: ['tests/e2e/*.js']
 };
